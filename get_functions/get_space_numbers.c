8c3bfabb7a5c136fb03e7468d98d89547cc5c747
mrouabeh <mohamed.rouabehi.pro@gmail.com>
2019-11-27 23:53:47 +0100

    get_space_number

--- get_functions/get_space_numbers.c
+++ get_functions/get_space_numbers.c
@@ -11,3 +11,13 @@
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
+
+int get_space_number(t_flags *flags, int nb_char)
+{
+	if (!flags || flags->width == -1)
+		return (0);
+	if (flags->width > nb_char)
+		return (flags->width - nb_char);
+	else
+		return (0);
+}
