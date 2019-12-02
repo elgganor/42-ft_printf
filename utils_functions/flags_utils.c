5571db4fc003d33388c69f7f48c3f426c2cc3ab9
mrouabeh <mrouabeh@student.42.fr>
2019-12-02 13:54:30 +0100

    Fichier flags_utils.c optimisé

--- utils_functions/flags_utils.c
+++ utils_functions/flags_utils.c
@@ -94,3 +94,31 @@ int	get_precision(const char *format, int *index, t_flags *flags)
 	free(precision);
 	return (i);
 }
+
+int get_precision_star(const char *format, int *index, t_flags *flags)
+{
+	char *precision;
+	int i;
+	int size;
+
+	i = *index;
+	size = 0;
+	if (format[i] == '-')
+	{
+		flags->flag = '-';
+		i++;
+		(*index)++;
+		return (-1);
+	}
+	while (ft_isdigit(format[i++]))
+		size++;
+	if (!(precision = (char *)malloc(sizeof(char) * (size + 1))))
+		return (-1);
+	i = 0;
+	while (ft_isdigit(format[*index]))
+		precision[i++] = format[(*index)++];
+	precision[i] = '\0';
+	i = ft_atoi(precision);
+	free(precision);
+	return (i);
+}
